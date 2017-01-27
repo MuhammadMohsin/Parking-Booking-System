45d492ebd40ae439ef70011b8154e152323ac94c
Mohsin <mmohsinkhalid@outlook.com>
2017-01-27 17:25:09 +0500

    Implemented car reserved feature

--- src/app/components/book.parking.component/book.parking.component.ts
+++ src/app/components/book.parking.component/book.parking.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AngularFire} from 'angularfire2'
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { UserService } from '../../services/user.service'
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {UserService} from '../../services/user.service'
 
 @Component({
   selector: 'login',
@@ -10,7 +10,7 @@ import { UserService } from '../../services/user.service'
 })
 export class BookParkingComponent {
   userObj = {email: "", password: ""};
-  afRef : any;
+  afRef: any;
   userAuth;
   router;
   userService;
@@ -25,16 +25,16 @@ export class BookParkingComponent {
     parkingDate: new Date(),
     startTime: "",
     parkingHrs: "",
-    location:"location1"
+    location: "location1"
   };
 
-  constructor(private af: AngularFire, private _router: Router, private _userService: UserService){
+  constructor(private af: AngularFire, private _router: Router, private _userService: UserService) {
     this.afRef = af;
     this.router = _router;
     this.userService = _userService;
     this.userAuth = this.userService.getUserData();
     this.locationsRef = this.afRef.database.list("/locations");
-    this.locationsRef.subscribe(locationsList=>{
+    this.locationsRef.subscribe(locationsList=> {
       this.AllLocations = locationsList;
 
       this.locationList1 = this.AllLocations[0];
@@ -44,31 +44,32 @@ export class BookParkingComponent {
       this.loadMaps = true;
     });
   }
-  /*loginUser(){
-    console.log(this.userObj);
-    if(this.userObj.email.trim() !="" && this.userObj.password.trim() !="")
-      this.afRef.auth.login({ email: this.userObj.email, password: this.userObj.password })
-        .then(auth => {
-          console.log(auth);
-          this.afRef.database.object("/users/"+auth.uid)
-            .subscribe(data=>{
-              this.userService.setUserData(data);
-              this.userAuth = data;
-              if(data.role =="user"){
-                alert("Go to user");
-                // this.router.navigate(["/jobs"]);
-              }
-              else{
-                alert("Go to admin");
-                // this.router.navigate(["/admin"])
-              }
-            });
 
-        }, function (err) {
-          console.log(err);
-          alert(err.message);
-        });
-  }*/
+  /*loginUser(){
+   console.log(this.userObj);
+   if(this.userObj.email.trim() !="" && this.userObj.password.trim() !="")
+   this.afRef.auth.login({ email: this.userObj.email, password: this.userObj.password })
+   .then(auth => {
+   console.log(auth);
+   this.afRef.database.object("/users/"+auth.uid)
+   .subscribe(data=>{
+   this.userService.setUserData(data);
+   this.userAuth = data;
+   if(data.role =="user"){
+   alert("Go to user");
+   // this.router.navigate(["/jobs"]);
+   }
+   else{
+   alert("Go to admin");
+   // this.router.navigate(["/admin"])
+   }
+   });
+
+   }, function (err) {
+   console.log(err);
+   alert(err.message);
+   });
+   }*/
 
   parseDate(dateString: string): Date {
     if (dateString) {
@@ -80,32 +81,73 @@ export class BookParkingComponent {
     }
   }
 
-  selectSlot(locationNum, slotObj, ind){
+  selectedSlot;
 
-    this.locationList1.slots.forEach(slot=>{
-      if(slot.bookedBy == "")
-          slot.selectable = "";
-    });
-    this.locationList2.slots.forEach(slot=>{
-      if(slot.bookedBy == "")
+  selectSlot(locationNum, slotObj, ind) {
+
+    this.locationList1.slots.forEach(slot=> {
+      if (slot.bookedBy == "")
         slot.selectable = "";
     });
-    this.locationList3.slots.forEach(slot=>{
-      if(slot.bookedBy == "")
+    this.locationList2.slots.forEach(slot=> {
+      if (slot.bookedBy == "")
         slot.selectable = "";
     });
-    slotObj.selectable = "true"
-  }
-    saveReservation(){
-    console.log(this.locationList1)
-    if(this.parkingObj.parkingDate != null && this.parkingObj.startTime.trim() != "" && this.parkingObj.parkingHrs.trim() != ""){
-      console.log(this.parkingObj);
+    this.locationList3.slots.forEach(slot=> {
+      if (slot.bookedBy == "")
+        slot.selectable = "";
+    });
+    slotObj.selectable = "true";
+
+    if (slotObj.bookedBy == "") {
+      this.selectedSlot = {
+        locationNum: locationNum,
+        slotInd: ind
+      };
+      console.log("pressed")
     }
-    else{
+    else {
+      this.selectedSlot = {
+        locationNum: "",
+        slotInd: ""
+      };
+      console.log("pressed")
+    }
+
+  }
+
+  saveReservation() {
+
+    if ((this.selectedSlot.locationNum !="" || this.selectedSlot.locationNum ==0) && this.parkingObj.parkingDate != null &&
+      this.parkingObj.startTime.trim() != "" && this.parkingObj.parkingHrs.trim() != "") {
+
+      console.log(this.parkingObj);
+      let key = "location"+this.selectedSlot.locationNum+"/slots/"+this.selectedSlot.slotInd;
+      this.locationsRef.update(key, {
+        bookedBy: this.userAuth.$key,
+        parkingData : this.parkingObj.parkingDate,
+        parkingHrs: this.parkingObj.parkingHrs,
+        startTime: this.parkingObj.startTime
+      }).then(data=>{
+        console.log(data);
+        this.parkingObj = {
+          parkingDate: new Date(),
+          startTime: "",
+          parkingHrs: "",
+          location: "location1"
+        };
+        alert("parking saved successfully")
+      }, err=>{
+        alert(err.message)
+      });
+
+    }
+    else {
       alert("Please fill all fields");
     }
   }
-  logout(){
+
+  logout() {
     this.userService.logoutUser();
   }
 }
